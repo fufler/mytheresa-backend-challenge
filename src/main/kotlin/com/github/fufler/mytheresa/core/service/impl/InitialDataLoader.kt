466ab41256420c9d7c0de7f436a9d30142f46ee9
Alexey Ermakov <fufler@gmail.com>
2022-03-22 10:49:13 +0300

    Introduce helper service to perform initial data loading

---
+++ src/main/kotlin/com/github/fufler/mytheresa/core/service/impl/InitialDataLoader.kt
@@ -0,0 +1,76 @@
+package com.github.fufler.mytheresa.core.service.impl
+
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.fufler.mytheresa.core.service.impl.InitialDataLoader.Companion.PROP_INITIAL_DATA_PATH
+import com.github.fufler.mytheresa.core.service.impl.model.CategoriesRepository
+import com.github.fufler.mytheresa.core.service.impl.model.DBBackedCategory
+import com.github.fufler.mytheresa.core.service.impl.model.DBBackedProduct
+import com.github.fufler.mytheresa.core.service.impl.model.ProductsRepository
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.io.File
+import javax.annotation.PostConstruct
+
+private data class Products(
+    val products: List<Product>
+) {
+    data class Product(
+        val name: String,
+        val sku: String,
+        val category: String,
+        val price: Int
+    )
+}
+
+
+@Service
+@ConditionalOnProperty(PROP_INITIAL_DATA_PATH)
+class InitialDataLoader(
+    private val categoriesRepository: CategoriesRepository,
+    private val productsRepository: ProductsRepository
+) {
+    @PostConstruct
+    @Transactional
+    fun load() {
+        val initialDataPath = System.getProperty(PROP_INITIAL_DATA_PATH) ?: return
+
+        if (categoriesRepository.isNotEmpty() || productsRepository.isNotEmpty())
+            return
+
+        val objectMapper = jacksonObjectMapper()
+
+        val products = File(initialDataPath).reader().use { reader ->
+            objectMapper.readValue<Products>(reader)
+        }.products
+
+        val categories = products
+            .map { it.category }
+            .distinct()
+
+        val categoriesMap = mutableMapOf<String, Long>()
+
+        for (category in categories)
+            categoriesMap[category] = categoriesRepository.save(
+                DBBackedCategory(
+                    name = category
+                )
+            ).id
+
+        for (product in products)
+            productsRepository.save(
+                DBBackedProduct(
+                    sku = product.sku,
+                    name = product.name,
+                    price = product.price,
+                    categoryId = categoriesMap.getValue(product.category)
+                )
+            )
+    }
+
+    companion object {
+        internal const val PROP_INITIAL_DATA_PATH = "db.initialDataPath"
+    }
+}
