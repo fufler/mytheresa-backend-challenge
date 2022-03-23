35614241a07fb2cfcc14f51a227f6cbc6dda17ef
Alexey Ermakov <fufler@gmail.com>
2022-03-23 10:54:51 +0300

    Use self-injection to resolve issue with @Transactional method invocation

--- src/main/kotlin/com/github/fufler/mytheresa/core/service/impl/InitialDataLoader.kt
+++ src/main/kotlin/com/github/fufler/mytheresa/core/service/impl/InitialDataLoader.kt
@@ -9,7 +9,9 @@ import com.github.fufler.mytheresa.core.service.impl.model.DBBackedCategory
 import com.github.fufler.mytheresa.core.service.impl.model.DBBackedProduct
 import com.github.fufler.mytheresa.core.service.impl.model.ProductsRepository
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.io.File
@@ -33,7 +35,15 @@ class InitialDataLoader(
     private val categoriesRepository: CategoriesRepository,
     private val productsRepository: ProductsRepository
 ) {
+    @Autowired
+    @Lazy
+    private lateinit var self: InitialDataLoader
+
     @PostConstruct
+    fun init() {
+        self.load()
+    }
+
     @Transactional
     fun load() {
         val initialDataPath = System.getProperty(PROP_INITIAL_DATA_PATH) ?: return
