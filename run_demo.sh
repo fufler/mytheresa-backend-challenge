287195d883ea8760122e4f080436d9f0cabd17a7
Alexey Ermakov <fufler@gmail.com>
2022-03-23 14:01:42 +0300

    Add script to run demo

---
+++ run_demo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+mvn                     \
+  -P docker             \
+  -D mvn.test.skip=true \
+  -D it.skip=true       \
+  clean verify
+
+./utils/generate_test_data.py    \
+  --categories-number 10000      \
+  --products-number 100000       \
+  --boots 20                     \
+   > demo/initial_data_100k.json
+
+docker-compose                   \
+  -f demo/docker-compose.yml     \
+  -p mytheresa-backend-challenge \
+  up                             \
+  --force-recreate               \
+  --renew-anon-volumes
